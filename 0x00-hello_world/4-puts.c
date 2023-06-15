e001eb18af9394f6b07cdc16d18d6f7947f517fd
MOUMOTD <touirid@gmail.com>
2023-06-15 16:05:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-* main - Printing
+* main - Printing without printf
 *
 * Return: Always 0 (Successful)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual 
-puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
