dbed27246506c2696a7432a1cb67c3e034c3c172
MOUMOTD <touirid@gmail.com>
2023-06-15 16:19:36 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - Printing msg using printf 
-*
-* Return: Always 0 (Successful)
+*main - Printing msg using printf
+*Return: Always 0 (Successful)
 */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("\"with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
