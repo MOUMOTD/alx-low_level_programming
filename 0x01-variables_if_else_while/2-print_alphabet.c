937033573da340f32c98174d731742f65edbb1ff
MOUMOTD <touirid@gmail.com>
2023-06-16 16:52:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Print the alphabet in lowercase using putchar
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	char letter = "a";
+
+	while (letter <= "z")
+
+	{
+		putchar(letter);
+		letter++;
+		letter++;
+		letter++;
+putchar("\n");
+retrun 0;
+}
