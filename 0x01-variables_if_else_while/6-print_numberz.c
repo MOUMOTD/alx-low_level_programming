0cee84389cfe23b20cf38f479dd73c7232d13284
MOUMOTD <touirid@gmail.com>
2023-06-19 07:28:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
