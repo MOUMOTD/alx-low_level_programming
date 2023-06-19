9bb23427393a6a3943ced570db022eb24ebdd33b
MOUMOTD <touirid@gmail.com>
2023-06-19 07:57:34 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print exad
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char m = '1';
+	char n = 'a';
+
+	while (m <= '9')
+	{
+		putchar(m);
+		m++;
+	}
+	while ( n <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
